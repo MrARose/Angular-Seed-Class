7e9c8fca180f38f9525fd5405d16ce90ac1229e4
Allan Rose <Allan.Rose@BadgerHat.ca>
2014-04-03 14:25:24 -0500

    Made changes as per class week 4

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,34 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
+  controller('MyCtrl1', ['$scope', function($scope) {
+      
+      $scope.car = {make: 'Honda',
+                    model: 'Civic',
+                    year: 2014,
+                    price: 24000
+                   }
   }])
-  .controller('MyCtrl2', [function() {
+  .controller('MyCtrl2', ['$scope', function($scope) {
 
+      $scope.cars = [{make: 'HondaA',
+                    model: 'Civic',
+                    year: 2014,
+                    price: 24000
+                   },
+                     {make: 'HondaB',
+                    model: 'Civic',
+                    year: 2014,
+                    price: 24000
+                     },
+                     {make: 'HondaC',
+                    model: 'Civic',
+                    year: 2014,
+                    price: 24000
+                   },
+                     {make: 'HondaD',
+                    model: 'Civic',
+                    year: 2014,
+                    price: 24000
+                   }]
   }]);
