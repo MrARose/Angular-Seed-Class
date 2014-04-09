5b16e204c564a713ff35fdb266b2e281e259ddd3
Allan Rose <Allan.Rose@BadgerHat.ca>
2014-04-09 14:27:05 -0500

    Added a whole buch of end-to-end test with protractor scripting

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -12,18 +12,52 @@ describe('my app', function() {
 
 
   describe('view1', function() {
+    var htmlElement;
+    var inElement;
+      
 
     beforeEach(function() {
       browser.get('index.html#/view1');
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
+    it('should render view1 when user navigates to /view1', function()     {
       expect(element.all(by.css('[ng-view] p')).first().getText()).
         toMatch(/partial for view 1/);
     });
 
-  });
+    function checkForField(fieldName,textVal,expectText,isShouldClearEdit) 
+      {
+        it('should have a "' + fieldName + '" label', function() {
+            htmlElement = element(by.id('lbl' + fieldName));
+            expect(htmlElement).not.toBe(undefined);
+            expect(htmlElement.getText()).toEqual(fieldName + ':');
+        });
+
+       it('should have a "' + fieldName + '" edit', function() {
+            htmlElement = element(by.id('ed' + fieldName));
+            expect(htmlElement).not.toBe(undefined);
+        });
+
+        it('should have a "' + fieldName + '" span', function() {
+            htmlElement = element(by.id('s' + fieldName));
+            expect(htmlElement).not.toBe(undefined);
+            var inElement = element(by.id('ed' + fieldName));
+            if(isShouldClearEdit)
+            {            
+                inElement.clear();
+            }
+            inElement.sendKeys(textVal);
+            expect(htmlElement.getText('value')).toEqual(expectText);
+        });
+      }
+
+    checkForField('Make','Dodge','Dodge',true);
+    checkForField('Model','Viper','Viper',true);
+    checkForField('Year','2012','2012',false);
+    checkForField('Price','99000','$99,000.00',true);
+
+});
 
 
   describe('view2', function() {
@@ -36,6 +70,21 @@ describe('my app', function() {
     it('should render view2 when user navigates to /view2', function() {
       expect(element.all(by.css('[ng-view] p')).first().getText()).
         toMatch(/partial for view 2/);
+    });
+
+    it('should have a list of cars and should have the correct number of rows', function() {
+        var rows = element.all(by.repeater('car in cars'));
+        expect(rows.count()).toBe(4);
+    });
+
+    it('should have "Honda" as the make in the first row', function() {
+        var rows = element.all(by.repeater('car in cars'));
+        rows.first().then( function(row) {
+            var rowItems = row.findElements(by.tagName('td'));
+            rowItems.then(function(cols) {
+               expect(cols[0].getText()).toMatch('HondaA');
+            });
+        });
     });
 
   });
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,16 +1,89 @@
 'use strict';
-
-/* jasmine specs for controllers go here */
-
 describe('controllers', function(){
-  beforeEach(module('myApp.controllers'));
+    
+    describe('MyCtrl1', function() {
+        var scope = {}, ctrl;
+        
+        beforeEach(module('myApp'));
+        
+        beforeEach(inject(function($controller) {
+            scope = {};
+            ctrl = $controller('MyCtrl1', {$scope: scope});
+        }));
+        
+        it('ctrl should exist', inject(function () {
+            
+            expect(ctrl).not.toBe(undefined);
+        }));
+        
+        it('car should exist', inject(function () {
+            expect(scope.car).not.toBe(undefined);
+        }));
+        
+        it('make should exist', inject(function () {
+            expect(scope.car.make).not.toBe(undefined);
+        }));
+        
+        it('model should exist', inject(function () {
+            expect(scope.car.model).not.toBe(undefined);
+        }));
+        
+        it('year should exist', inject(function () {
+            expect(scope.car.year).not.toBe(undefined);
+        }));
+        
+        it('price should exist', inject(function () {
+            expect(scope.car.price).not.toBe(undefined);
+        }));
+        
+        it('make should be Honda', inject(function () {
+            expect(scope.car.make).toBe('Honda');
+        }));
+        
+        it('model should be Civic', inject(function () {
+            expect(scope.car.model).toBe('Civic');
+            
+        }));
+        
+    });
 
+    describe('MyCtrl2', function() {
+        var scope = {}, ctrl;
+        
+        beforeEach(module('myApp'));
+        
+        beforeEach(inject(function($controller) {
+            scope = {};
+            ctrl = $controller('MyCtrl2', {$scope: scope});
+        }));
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+        it('ctrl2 should exist', inject(function () {
+            
+            expect(ctrl).not.toBe(undefined);
+        }));
+        
+        it('car should exist', inject(function () {
+            expect(scope.cars).not.toBe(undefined);
+        }));
+        
+        it('should have 6 cars', inject(function () {
+            expect(scope.cars.length).toBe(4);
+        }));
+        
+        it('should have a new car (5 total)', inject(function () {
+            scope.cars.push({make: 'HondaX',
+                    model: 'X Car',
+                    year: 2015,
+                    price: 99000
+                     });
+        
+            expect(scope.cars.length).toBe(5);
+        }));
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+        it('car 2 should be HondaB', inject(function () {
+            expect(scope.cars[1].make).toBe('HondaB');
+        }));
+
+    });
+        
 });
